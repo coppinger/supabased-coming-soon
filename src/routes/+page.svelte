ba7d8d7154fbc6d4b8f03177ac55ebaaea69eb0a
Charlie Coppinger <charles.coppinger@gmail.com>
2024-04-25 16:03:56 +1200

    get in placeholder content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center items-center min-h-screen min-w-screen bg-neutral-950">
+	<pre
+		class="text-green-400 text-[6px] md:text-xs font-bold"
+		style="white-space: pre; font-family: monospace; letter-spacing: unset; transform: unset; overflow-y: hidden;">
+    ___                                   _                                          _   
+   F __".  _    _    _ ___      ___ _    FJ___      ___ _     ____      ____      ___FJ  
+  J (___| J |  | L  J '__ J    F __` L  J  __ J    F __` L   F ___J    F __ J    F __  L 
+  J\___ \ | |  | |  | |--| |  | |--| |  | |--| |  | |--| |  | '----_  | _____J  | |--| | 
+ .--___) \F L__J J  F L__J J  F L__J J  F L__J J  F L__J J  )-____  L F L___--. F L__J J 
+ J\______J\____,__LJ  _____/LJ\____,__LJ__,____/LJ\____,__LJ\______/FJ\______/FJ\____,__L
+  J______FJ____,__F|_J_____F  J____,__FJ__,____F  J____,__F J______F  J______F  J____,__F
+                   L_J                                                                   
+</pre>
+</div>
